ceac9a8b7e6eefd7ba156329bb04b0dd1c800104
Marcin Biela <marcin.biela12@gmail.com>
2020-09-06 15:37:00 +0200

    Poprawa 1

--- Dodawanie_poziomu_cisnienia/add_new_position.py
+++ Dodawanie_poziomu_cisnienia/add_new_position.py
@@ -3,17 +3,19 @@
 
 
 def input_data():
+    # Data - dzien.miesiac.rok
+
     date_input = input("Podaj date:")
-    blood_pressure_input = input("Podaj cisnienie krwi:")
-    systolic_pressure_input = input("Podaj cisnienie skurczowe")
-    diastolic_pressure_input = input("Podaj cisnienie rozkurczowe")
-    return [date_input, blood_pressure_input, systolic_pressure_input, diastolic_pressure_input]
+    systolic_pressure_input = input("Podaj cisnienie skurczowe:")
+    diastolic_pressure_input = input("Podaj cisnienie rozkurczowe:")
+    blood_pressure_input = input("Podaj puls:")
+    return [date_input, systolic_pressure_input, diastolic_pressure_input, blood_pressure_input]
 
 
-def add_data(date_input, blood_pressure_input, systolic_pressure, diastolic_pressure):
+def add_data(date_input, systolic_pressure, diastolic_pressure, blood_pressure_input):
     measurement_database = list()
     measurement_database.append(date_input)
-    measurement_database.append(blood_pressure_input)
     measurement_database.append(systolic_pressure)
     measurement_database.append(diastolic_pressure)
+    measurement_database.append(blood_pressure_input)
     return measurement_database
--- Dodawanie_poziomu_cisnienia/file_mode.py
+++ Dodawanie_poziomu_cisnienia/file_mode.py
@@ -9,11 +9,11 @@ def write_data():
     f = open(filepath, 'a', encoding="utf-8")
     x = input_data()
     date = x[0]
-    blood_pressure = x[1]
-    systolic_pressure = x[2]
-    diastolic_pressure = x[3]
+    systolic_pressure = x[1]
+    diastolic_pressure = x[2]
+    blood_pressure = x[3]
 
-    data = add_data(date, blood_pressure, systolic_pressure, diastolic_pressure)
+    data = add_data(date, systolic_pressure, diastolic_pressure, blood_pressure)
     string = ""
     for i in data:
         string += i
@@ -46,16 +46,18 @@ def load_data():
 # Działa
 def data_unpack():
     date = []  # Tworze osobne kontenery na date i wartosc cisnienia
-    blood_pressure = []
     systolic_pressure = []
     diastolic_pressure = []
+    blood_pressure = []
+
     x = load_data()  # Wczytywanie wartosci
     for i in x:
         date.append(i[0])
-        blood_pressure.append(int(i[1]))
-        systolic_pressure.append(int(i[2]))
-        diastolic_pressure.append(int(i[3]))
-    value = [blood_pressure, systolic_pressure, diastolic_pressure]
+        systolic_pressure.append(int(i[1]))
+        diastolic_pressure.append(int(i[2]))
+        blood_pressure.append(int(i[3]))
+
+    value = [systolic_pressure, diastolic_pressure, blood_pressure]
     return date, value  # Zwraca krotke tablic (data: str, wartosc: [int, int, int])
 
 
@@ -63,25 +65,25 @@ def search_by_date():
     temp_list = []
     searching_date = input("Podaj date: ")
     date, blood_pressure_list = data_unpack()
-    blood_pressure = blood_pressure_list[0]
-    systolic_pressure = blood_pressure_list[1]
-    diastolic_pressure = blood_pressure_list[2]
+    systolic_pressure = blood_pressure_list[0]
+    diastolic_pressure = blood_pressure_list[1]
+    blood_pressure = blood_pressure_list[2]
+
     n = len(date)
 
     for i in range(n):
         if searching_date == date[i]:
-            temp_list.append(blood_pressure[i])
             temp_list.append(systolic_pressure[i])
             temp_list.append(diastolic_pressure[i])
-
+            temp_list.append(blood_pressure[i])
     return temp_list if temp_list != [] else "Nie ma takiej daty"
 
 
 def search_by_value():
     temp_list = []
-    searching_value = int(input("Podaj wartosc cisnienia krwi: "))
+    searching_value = int(input("Podaj wartosc pulsu: "))
     date, blood_pressure_list = data_unpack()
-    blood_pressure = blood_pressure_list[0]
+    blood_pressure = blood_pressure_list[2]
     n = len(blood_pressure)
 
     for i in range(n):
@@ -89,6 +91,5 @@ def search_by_value():
             temp_list.append(date[i])
 
     return temp_list if temp_list != [] else "Nie ma takiej wartosci"
-
 
 
