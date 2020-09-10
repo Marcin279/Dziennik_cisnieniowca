a057a417e4d9e72786a624a27e0b3ed79e829483
MarcinBiela <56258793+Marcin279@users.noreply.github.com>
2020-09-10 17:35:59 +0200

    Poprawa2_diagram

--- Dodawanie_poziomu_cisnienia/diagram.py
+++ Dodawanie_poziomu_cisnienia/diagram.py
@@ -2,54 +2,39 @@
 # -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
-import file_mode
+from file_mode import data_unpack
 
-x = file_mode.data_unpack()
+x = data_unpack()
 
 date = x[0]
 values = x[1]
 
 
-def get_month():
-    temp = []
-    for i in date:
-        temp.append(i.split('.'))
-
-    month = []
-    for j in temp:
-        month.append(int(j[1]))
-
-    return month  # list(int)
-
-
 # # TODO: Do usunięcia, używane do testów
 # selected_month = int(input("Podaj miesiac, z ktorego chcesz miec wyniki: "))
 
-
-# Funkcja otrzymuje jako parametr miesiąc i rysuje wykres z podanego miesiąca
-def plot_diagram(month: int):
+# Funkcja otrzymuje jako parametr miesiąc i rok i rysuje wykres z podanego okresu
+def plot_diagram(dates):
     systolic_pressure = values[0]
     diastolic_pressure = values[1]
     blood_pressure = values[2]
-    i = 0
+    j = 0
     sys = []
     blood = []
     dias = []
-    dates = []
-    for mon in get_month():
-        if month == mon:
-            dates.append(date[i])
-            sys.append(systolic_pressure[i])
-            dias.append(diastolic_pressure[i])
-            blood.append(blood_pressure[i])
-            # plt.plot(date[i], systolic_pressure[i], 'go', label=)
-            # plt.plot(date[i], diastolic_pressure[i], 'ro')
-            # plt.plot(date[i], blood_pressure[i], 'bo')
-        i += 1
+    dates_list = []
 
-    plt.plot(dates, sys, 'o', label="Cisnienie skurczowe")
-    plt.plot(dates, dias, 'ro', label="Cisnienie rozkurczowe")
-    plt.plot(dates, blood, 'bo', label="Puls")
+    for i in date:
+        if dates == i[3:]:
+            dates_list.append(date[j])
+            sys.append(systolic_pressure[j])
+            dias.append(diastolic_pressure[j])
+            blood.append(blood_pressure[j])
+        j += 1
+
+    plt.plot(dates_list, sys, 'o', label="Cisnienie skurczowe")
+    plt.plot(dates_list, dias, 'ro', label="Cisnienie rozkurczowe")
+    plt.plot(dates_list, blood, 'bo', label="Puls")
     plt.title("Wykres")
     plt.xlabel("Data")
     plt.ylabel("Wartosci cisnienia")
@@ -57,3 +42,4 @@ def plot_diagram(month: int):
     plt.show()
 
 
+plot_diagram('09.20')
